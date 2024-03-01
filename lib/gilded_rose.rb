1b692845632e3b7fcac7969a767aa9e0ce0a175f
Lakshman-nunna <saibajinunna3@gmail.com>
2024-02-29 22:14:14 -0600

    the sulfuras method does not have anything which we could not identify it in the old form

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -9,25 +9,32 @@ class GildedRose
 
   def tick
 
+
+    def normal_tick
+      @days_remaining -= 1
+      return if @quality == 0
+      @quality -= 1
+      @quality -= 1 if @days_remaining <= 0
+      end
+
+      def brie_tick
+        @days_remaining -= 1
+        return if @quality >= 50
+        @quality += 1
+        @quality += 1 if @days_remaining <= 0 && @quality < 50
+      end
+
     case name
     when 'Normal Item'
       return normal_tick
     when 'Aged Brie'
       return brie_tick
+    when 'Sulfurus, Hand of Ragnaros'
+      return sulfuras_tick
     end
 
-    def normal_tick
-    @days_remaining -= 1
-    return if @quality == 0
-    @quality -= 1
-    @quality -= 1 if @days_remaining <= 0
-    end
 
-    def brie_tick
-      @days_remaining -= 1
-      return if @quality >= 50
-      @quality += 1
-      @quality += 1 if @days_remaining <= 0 && @quality < 50
+    def sulfuras_tick
     end
 
 
