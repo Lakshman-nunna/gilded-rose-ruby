433ed0598107af16738d6c67ee9999ef2e38b331
Lakshman-nunna <saibajinunna3@gmail.com>
2024-02-29 21:58:45 -0600

    With the brie method, 7 tests fail

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -9,8 +9,11 @@ class GildedRose
 
   def tick
 
-    if name == 'Normal Item'
+    case name
+    when 'Normal Item'
       return normal_tick
+    when 'Aged Brie'
+      return brie_tick
     end
 
     def normal_tick
@@ -20,6 +23,9 @@ class GildedRose
     @quality -= 1 if @days_remaining <= 0
     end
 
+    def brie_tick
+
+    end
 
 
     if @name != "Aged Brie" and @name != "Backstage passes to a TAFKAL80ETC concert"
